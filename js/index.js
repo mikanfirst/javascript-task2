36cdee35556c152f21f1dd6678c7cd363530480c
mikanfirst <mmkkmkk4869@gmail.com>
2022-03-03 02:27:39 +0900

    [追加]入力された値の処理

--- js/index.js
+++ js/index.js
@@ -1,4 +1,51 @@
-let element = document.getElementById("quiz_1");
-let elements = element.answer
-elements[1].checked = true ;
-elements[1].checked = false ;
+
+let score = 0;
+  
+function showScore() {
+
+  const QUIZ_1 = document.getElementById("quiz_1");
+  if (QUIZ_1.answer.value == 'b') {
+    score++;
+  }
+
+  const QUIZ_2 = document.getElementById("quiz_2");
+  if (QUIZ_2.answer.value == `b`) {
+    score++;
+  }
+
+  const QUIZ_3 = document.getElementById("quiz_3");
+  if (QUIZ_3.answer.value == `a`) {
+    score++;
+  }
+
+  const QUIZ_4 = document.getElementById("quiz_4");
+  if (QUIZ_4.answer.value == `b`) {
+    score++;
+  }
+
+  const QUIZ_5 = document.getElementById("quiz_5");
+  if (QUIZ_5.answer.value == `a`) {
+    score++;
+  }
+
+  const totalScore = document.getElementById('score_message');
+
+  if (score === 5) {
+    totalScore.innerHTML = score + "点満点：秀才の極みだね！！";
+  } else if(score === 4) {
+    totalScore.innerHTML = score + "点：惜しかったね！悔しい？";
+  } else if(score === 3) {
+    totalScore.innerHTML = score + "点：平均です！";
+  } else if(score === 2) {
+    totalScore.innerHTML = score + "点：頑張れ！";
+  } else if(score === 1) {
+    totalScore.innerHTML = score + "点：残念！";
+  } else {
+    totalScore.innerHTML = score + "点：修行が足りん！！";
+  }
+
+}
+
+// const correct = "正解です"
+// const incorrect = "不正解です"
+
